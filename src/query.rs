5d9e84507c537c2f3b0b2c364401e98b1fa683ab
Max Tyler <maxastyler@gmail.com>
2019-02-03 01:34:26 +0000

    Add query module and some tests for the query builder

---
+++ src/query.rs
@@ -0,0 +1,79 @@
+use serde_yaml::Value;
+
+/// QueryData is the set of strings that are to be searched through
+/// The given strings are built from a `Vec<Vec<&str>>` where the inner vector
+/// contains the terms which are to be grouped together into the different strings
+/// and the outer vector contains the number of different strings that will be built
+#[derive(Debug, PartialEq)]
+pub struct QueryData {
+    strings: Vec<String>,
+}
+
+impl QueryData {
+    /// Build a `QueryData` entry from a `serde_yaml` `Mapping` (containing the reference data) and a `Vec` containing `Vec<&str>` which denote the arguments to be combined for each element
+    pub fn build(entry: &Value, arguments: &Vec<Vec<&str>>) -> QueryData {
+        let strings: Vec<String> = arguments
+            .iter()
+            .map(|strings| {
+                let mut s = strings
+                    .iter()
+                    .map(|s| match entry.get(s) {
+                        Some(v) => v.as_str().unwrap_or(""),
+                        None => "",
+                    })
+                    .collect::<Vec<&str>>()
+                    .join("\n");
+                s.truncate(1024); // truncate search string to 1024 characters, as rff has that as a maximum
+                s
+            })
+            .collect();
+        QueryData { strings: strings }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::QueryData;
+    use serde_yaml::{Mapping, Value};
+    use std::iter::FromIterator;
+
+    #[test]
+    fn querydata_builds_correctly_multiples() {
+        let v = Value::Mapping(Mapping::from_iter(vec![
+            (
+                Value::String(String::from("title")),
+                Value::String(String::from("a test title")),
+            ),
+            (
+                Value::String(String::from("author")),
+                Value::String(String::from("arthur grunp")),
+            ),
+        ]));
+        let query = QueryData::build(
+            &v,
+            &vec![vec!["title", "author"], vec!["author", "title", "title"]],
+        );
+        assert_eq!(
+            query,
+            QueryData {
+                strings: vec![
+                    String::from("a test title\narthur grunp"),
+                    String::from("arthur grunp\na test title\na test title")
+                ]
+            }
+        )
+    }
+
+    #[test]
+    fn querydata_build_truncates_to_1024_chars() {
+        let long_str = Value::String((0..1050).map(|_| 'a').collect::<String>());
+        let shorter_str = Value::String((0..1024).map(|_| 'a').collect::<String>());
+        let v = Value::Mapping(Mapping::from_iter(vec![(
+            Value::String(String::from("long")),
+            long_str,
+        )]));
+        let query = QueryData::build(&v, &vec![vec!["long", "long"]]);
+        assert_eq!(query.strings, vec![shorter_str]);
+    }
+
+}
